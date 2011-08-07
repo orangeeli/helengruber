f0e890f6e323770eb3c646362652bb9a41b559d4
Eliseu Martinho <eliseu.martinho@gmail.com>
2011-08-07 15:06:06 +0100

    Image Upload (raw behavior)

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -5,12 +5,15 @@
  * Time: 8:19 PM
  * To change this template use File | Settings | File Templates.
  */
+file_input_has_file = null;
 $(document).ready(function() {
     // go through elements with class add a function
     // load the menu functions
 
     // should be done only once in the application
     Configuration.setMenuActions();
+    // Configuration.setFormBehavior();
+    initConfig();
 });
 Configuration = {
     sub_menu_section : '#sub_menu',
@@ -64,18 +67,50 @@ Configuration = {
             }
         });
     },
-
-    setFormBehavior : function(){
-      
-      $('form').each(function(){
-        // add on submit behavior
-        // add on iframe loaded behvior
-        // force page refresh
-         
-      }):
-    }
+    setFormBehavior : initConfig()
 
 };
+
+function initConfig(){
+      // add regex for form name convention
+      $('form').each(function(){
+        // add on submit behavior
+        input_file = $(this).find('input[type="file"]');
+        input_file.change(function(){onFileChanged();});
+        $(this).append('<input type="hidden" value="" class="file_has_changed" />');
+        
+        file_upload_form = this;
+        $('input[type="submit"]').click(function(){onformSubmit(file_upload_form, input_file);});
+      });
+    };
+
+function onFileChanged(){
+  $('.file_has_changed').val(1);
+}
+
+function onformSubmit(file_upload_form, input_file){
+
+  // search for the input and hide it
+  // show on loading message
+  // input_file = $(file_upload_form).find('input[type="file"]');
+  input_file.hide();
+  
+  div_actions = $(file_upload_form).find('.actions');
+  div_actions.hide();
+  // inject with client side loading feedback element
+  
+  $(file_upload_form).append('<div class="upload_feedback">uploading image...</div>');
+
+  $('iframe').each(function(){
+        this.onload = onframeloaded();
+  });
+
+}
+function onframeloaded(){
+  // either refresh page or show or inject image (that would require json response) => do it. Return a json object
+  $('.upload_feedback').text("Image uploaded");
+}
+
 Ajax = {
     htmlDataType : 'html',
     call : function(url){
