2fc97113f4ce7b90dd51fac93c5323fc6442ebbb
Eliseu Martinho <eliseu.martinho@gmail.com>
2011-08-21 19:33:40 +0100

    Forms layout

--- public/javascripts/pageWorker.js
+++ public/javascripts/pageWorker.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     //Configuration.setMenuActions();
     fileUploadBuilder = new FileUploadFormBuilder;
     fileUploadBuilder.buildForm('file_upload');
+    initFileUploads();
 });
 
 // page worker - will config the elements with the necessary logic and ui
@@ -52,3 +53,32 @@ function onframeloaded(iframe){
     window.location.reload();
   }
 }
+
+var W3CDOM = (document.createElement && document.getElementsByTagName);
+
+function initFileUploads() {
+	if (!W3CDOM) return;
+	var fakeFileUpload = document.createElement('div');
+	fakeFileUpload.className = 'fakefile';
+        var fakeInput = document.createElement('input');
+        fakeInput.className = 'fake_input';
+        var fakeInputButton = document.createElement('input');
+        fakeInputButton.type = 'button'; 
+        fakeInputButton.value = 'Choose File';
+	fakeFileUpload.appendChild(fakeInput);
+        fakeFileUpload.appendChild(fakeInputButton);
+	var x = document.getElementsByTagName('input');
+	for (var i=0;i<x.length;i++) {
+		if (x[i].type != 'file') continue;
+		if (x[i].parentNode.className != 'field file_inputs') continue;
+		//x[i].className = 'file hidden';
+		var clone = fakeFileUpload.cloneNode(true);
+		x[i].parentNode.appendChild(clone);
+		x[i].relatedElement = clone.getElementsByTagName('input')[0];
+		x[i].onchange = x[i].onmouseout = function () {
+			this.relatedElement.value = this.value;
+		}
+	}
+}
+
+
